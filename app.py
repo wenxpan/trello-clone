09d1f9ad827947d962bfa8446b35d16b1a9cec50
wenxuan-pan <panwenxuan42@gmail.com>
2023-05-31 12:08:52 +1000

    created Card model and create_all CLI command

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@127.0.0.1:5432/trello'
+
+db = SQLAlchemy(app)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text())
+
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print('Tables created successfully')
 
 @app.route('/')
 def index():

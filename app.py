38f953c1f42df9944dc33957c99dd69ad3ca41dd
wenxuan-pan <panwenxuan42@gmail.com>
2023-06-01 12:12:11 +1000

    w2d4 lesson

--- app.py
+++ app.py
@@ -11,21 +11,25 @@ db = SQLAlchemy(app)
 
 ma = Marshmallow(app)
 
+
 class Card(db.Model):
     __tablename__ = 'cards'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
+    status = db.Column(db.String(30))
     date_created = db.Column(db.Date())
 
 
 class CardSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'title', 'description', 'date_created')
+        fields = ('id', 'title', 'description', 'status', 'date_created')
+
 
 card_schema = CardSchema()
 cards_schema = CardSchema(many=True)
+
 
 @app.cli.command('create')
 def create_db():
@@ -37,32 +41,47 @@ def create_db():
 @app.cli.command('seed')
 def seed_db():
     # create an instance of the Card model in memory
-    card = Card(
-        title = 'Start the project',
-        description = 'Stage 1 - Create an ERD',
-        date_created = date.today()
-    )
+    cards = [Card(
+        title='Start the project',
+        description='Stage 1 - Create an ERD',
+        status="Done",
+        date_created=date.today()
+    ),
+        Card(
+        title='ORM queries',
+        description='Stage 2 - Implement several queries',
+        status='In Progress',
+        date_created=date.today()
+    ),
+        Card(
+        title='Marshmellow',
+        description='Stage 3 - Implement jsonify of models',
+        status='In Progress',
+        date_created=date.today()
+    ),]
 
-    # truncate table
+    # truncate table; delete all rows in the table
     db.session.query(Card).delete()
 
     # add the card to the session (transaction)
-    db.session.add(card)
+    db.session.add_all(cards)
 
     # commit the transaction to the database
     db.session.commit()
     print('Models seeded')
 
+
+@app.route('/cards')
+def all_cards():
+    stmt = db.select(Card).order_by(Card.status.desc())
+    cards = db.session.scalars(stmt).all()
+    return cards_schema.dump(cards)
+
+
 @app.route('/')
 def index():
     return 'Hello World!'
 
-
-@app.route('/cards', methods=['GET'])
-def get_cards():
-    cards_list = Card.query.all()
-    result = cards_schema.dump(cards_list)
-    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
